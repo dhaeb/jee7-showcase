2109a97d2e99296ca2ca5d76eef761656059f749
commic <michael.weber04@gmail.com>
2014-06-22 19:41:49 +0200

    Initialer Commit
    CRM-Demo mit Bootstrap 3 und Angular 1.2.16

---
+++ src/main/webapp/app/js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, services and directives
+angular.module('CrmDemo', ['CrmDemo.controllers', 'CrmDemo.filters', 'CrmDemo.services', 'CrmDemo.directives', 'ngRoute','ngResource' ])
+        .config([ '$routeProvider', function($routeProvider) {
+            $routeProvider.when('/list', {
+                templateUrl : 'app/partials/customer-list.html',
+                controller : 'CustomerListCtrl'
+            });
+            $routeProvider.when('/edit/:customerId', {
+                templateUrl : 'app/partials/customer-detail.html',
+                controller : 'CustomerDetailCtrl'
+            });
+            $routeProvider.when('/new', {
+                templateUrl : 'app/partials/customer-detail.html',
+                controller : 'CustomerDetailCtrl'
+            });
+            $routeProvider.otherwise({
+                redirectTo : '/list'
+            });
+        } ]);
